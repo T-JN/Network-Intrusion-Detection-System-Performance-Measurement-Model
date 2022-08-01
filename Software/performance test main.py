346a6d2ca0ff96c0298eeb3abd25a1f08069e2b7
T-J2 <97032073+T-JN@users.noreply.github.com>
2022-08-01 21:41:33 +0400

    software&manual

---
+++ Software/performance test main.py
@@ -0,0 +1,39 @@
+
+Tests for benchmark.py.
+from __future__ import print_function
+
+
+
+class TestBenchmarkRunner(unittest.TestCase):
+
+  def test_get_benchmark_methods_filter(self):
+    """Tests returning methods on a class based on a filter."""
+    config = mock.Mock()
+    config.workspace = 'workspace'
+    config.benchmark_method_patterns = ['new_foo.BenchmarkClass.filter:bench.*']
+    benchmark_runner = benchmark.BenchmarkRunner(config)
+
+    mock_benchmark_class = mock.Mock()
+    mock_benchmark_class.benchmark_method_1 = 'foo'
+
+    mock_module = mock.Mock()
+    sys.modules['new_foo'] = mock_module
+    mock_module.BenchmarkClass.return_value = mock_benchmark_class
+
+    methods = benchmark_runner._get_benchmark_methods()
+
+    self.assertEqual(1, len(methods))
+    self.assertEqual('new_foo.BenchmarkClass.benchmark_method_1', methods[0])
+
+  def test_get_benchmark_methods_exact_match(self):
+    """Tests returning methods on a class based on a filter."""
+    config = mock.Mock()
+    config.workspace = 'workspace'
+    config.benchmark_method_patterns = [
+        'new_foo.BenchmarkClass.benchmark_method_1',
+        'new_foo.BenchmarkClass.benchmark_method_2']
+    benchmark_runner = benchmark.BenchmarkRunner(config)
+
+    methods = benchmark_runner._get_benchmark_methods()
+    self.assertEqual(['new_foo.BenchmarkClass.benchmark_method_1',
+                      'new_foo.BenchmarkClass.benchmark_method_2'], methods)
