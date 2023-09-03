c7b316079cd8dfb3bdb781204d6518a863855da7
Timur Jamgharyan <97032073+T-JN@users.noreply.github.com>
2023-09-03 11:52:58 +0400

    Update Setup.py

--- Software/Setup.py
+++ Software/Setup.py
@@ -1,7 +1,7 @@
 
 
 
-import json                       #malware files to  20,40,80,128,256,512,1024 byte size
+import json                       #malware files to  20,40,80,128,256,512,1024 byte  size
 import logging
 import  multiprocess ing
 import os                   # Ubuntu
